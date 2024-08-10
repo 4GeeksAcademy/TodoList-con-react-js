b6b377546ee02aee3f769138159f406f089ba70f
Amanda Dias <168840059+mnddevl@users.noreply.github.com>
2024-08-10 09:41:12 +0000

    cambios en el css, contador...

--- src/js/component/ToDoList.jsx
+++ src/js/component/ToDoList.jsx
@@ -3,24 +3,32 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
 function ToDoList() {
-    const [tasks, setTasks] = useState([]);
-    const [newTask, setNewTask] = useState("");
+    const [tarea, setTarea] = useState([]);
+    const [nuevaTarea, setNuevaTarea] = useState("");
 
     function handleInputChange(event) {
-        setNewTask(event.target.value);
+        setNuevaTarea(event.target.value);
     }
 
-    function addTask() {
-        if (newTask.trim() !== "") {
-            setTasks(t => [...t, newTask])
-            setNewTask("");
+    function addTarea() {
+        if (nuevaTarea.trim() !== "") {
+            setTarea(t => [...t, nuevaTarea])
+            setNuevaTarea("");
         }
     }
 
-    function deleteTask(index) {
-        const updatesTasks = tasks.filter((_, i) => i !== index);
-        setTasks(updatesTasks);
+    function eliminarTarea(index) {
+        const updatesTarea = tarea.filter((_, i) => i !== index);
+        setTarea (updatesTarea);
     }
+
+    function handleKeyDown(event) {
+        if (event.key === 'Enter') {
+            addTarea();
+        }
+    }
+
+    const taskText = tarea.length === 1 ? 'tarea' : 'tareas';
 
     return (
         <div className="to-do-list">
@@ -29,25 +37,27 @@ function ToDoList() {
             <div>
                 <input
                     type="text"
-                    placeholder="Adicionar tarea"
-                    value={newTask}
+                    placeholder="Añadir tarea"
+                    value={nuevaTarea}
                     onChange={handleInputChange}
+                    onKeyDown={handleKeyDown}
                 />
-                <button className='Add-button'
-                    onClick={addTask}>
+                <button className='add-button'
+                    onClick={addTarea}>
                     Add
                 </button>
             </div>
             <ul>
-                {tasks.map((task, index) =>
+                {tarea.map((tarea, index) =>
                     <li key={index}>
-                        <span className='text'> {task} </span>
-                        <button className='deleteButton' onClick={() => deleteTask(index)}>
+                        <span className='text'> {tarea} </span>
+                        <button className='deleteButton' onClick={() => eliminarTarea(index)}>
                         <FontAwesomeIcon icon={faTrash} />
                         </button>
                     </li>
                 )}
             </ul>
+            <p className='contador'>Tienes {tarea.length} {taskText}</p>
         </div>
     );
 }
